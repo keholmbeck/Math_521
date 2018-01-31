cca320166463a5182f83627374c2f51940314673
Kristin Holmbeck <keholmbeck@users.noreply.github.com>
2018-01-31 12:59:11 -0800

    Adding prinAngles.m -- still need to check

---
+++ HW1/prinAngles.m
@@ -0,0 +1,35 @@
+function theta = prinAngles(X1,X2)
+
+% Find orthonormal bases for X1,X2
+% [Q1,R1] = qr(X1);
+% [Q2,R2] = qr(X2);
+Q1 = orth(X1);
+Q2 = orth(X2);
+
+% Compute SVD for cosine
+[U,S,V] = svd(Q1' * Q2);
+S = diag(S);
+
+% Compute matrix Y
+if rank(Q1) >= rank(Q2)
+    Y = Q2 - Q1*(Q1'*Q2);
+else
+    Y = Q1 - Q2*(Q2'*Q1);
+end
+
+% SVD for sine
+[U,M,V] = svd(Y,0);
+M = diag(M);
+
+% Compute the principal angles for k = 1,...,q
+theta = zeros(size(X2,2), 1);
+for ii = 1:length(theta)
+    if S(ii)^2 < 1/2
+        theta(ii) = acos(S(ii));
+    end
+    if M(ii)^2 <= 1/2
+        theta(ii) = acos(M(ii));
+    end
+end
+
+end
--- HW1/testIt.m
+++ HW1/testIt.m
@@ -20,3 +20,29 @@ for ii = 1:length(x)
 end
 hold off;
 
+
+clear;
+
+load face1;
+load face2;
+
+theta = prinAngles(face1, face2);
+theta = 180/(2*pi)*theta;
+
+for ii = 1:size(face1,2)
+    subplot(131);
+    imagesc(reshape(face1(:,ii),160,138)), colormap(gray), axis off
+    
+    subplot(132);
+    imagesc(reshape(face2(:,ii),160,138)), colormap(gray), axis off
+    title(sprintf('Iteration %d', ii), 'FontW', 'B');
+    
+    subplot(133);
+    plot(theta); hold on; plot(ii, theta(ii), 'or'); hold off;
+    ylabel 'degrees';
+    
+    drawnow;
+    pause(0.5);
+end
+
+hold off;
